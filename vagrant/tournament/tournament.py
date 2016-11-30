b166030da1de9d8fccf78b1c0378ab9eeb54ffc6
shuminyang <shu.minyang@hotmail.com>
2016-11-30 17:58:18 -0200

    commit

--- vagrant/tournament/tournament.py
+++ vagrant/tournament/tournament.py
@@ -10,6 +10,21 @@ DELETE_ALL_PLAYER = "DELETE FROM player"
 COUNT_PLAYER = "SELECT COUNT(*) FROM player"
 INSERT_MATCH = "INSERT INTO MATCHES (winner, loser) values (%s, %s)"
 DELETE_ALL_MATCHES = "DELETE FROM matches"
+PLAYER_STANDINGS = ("SELECT  pid AS player_id, name, COALESCE(win_count, 0) AS total_win, total_match FROM "
+                        "(SELECT pl.player_id AS pid, pl.name AS name, count(*) AS total_match FROM "
+                                "player pl,"
+                                "matches mat "
+                                "WHERE mat.winner = pl.player_id "
+                                "OR    mat.loser = pl.player_id "
+                                "GROUP BY pl.player_id "
+                                "ORDER BY pl.player_id) AS tm "
+                        "LEFT JOIN "
+                        "(SELECT pl.player_id AS plid, win_count FROM "
+                                "player pl, "
+                                "(SELECT winner, count(*) AS win_count FROM matches GROUP BY winner) AS m "
+                                "WHERE m.winner = pl.player_id) AS wm "
+                        "ON tm.pid = wm.plid "
+                        "ORDER BY total_win desc;")
 
 def connect():
     """Connect to the PostgreSQL database.  Returns a database connection."""
@@ -80,6 +95,14 @@ def playerStandings():
         wins: the number of matches the player has won
         matches: the number of matches the player has played
     """
+    conn = connect()
+    cursor = conn.cursor()
+    cursor.execute(PLAYER_STANDINGS)
+    result_count = cursor.fetchall()
+
+    cursor.close()
+    conn.close()
+    return result_count
 
 
 def reportMatch(winner, loser):
@@ -114,6 +137,35 @@ def swissPairings():
         id2: the second player's unique id
         name2: the second player's name
     """
+    conn = connect()
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM matches")
+    result_count = cursor.fetchall()
+
+    if not result_count:
+        cursor.execute("SELECT * FROM player")
+        result_count = cursor.fetchall()
+        i = 0
+        list_tup = []
+        while i < len(result_count):
+            tup = (result_count[i][0], result_count[i][1], result_count[i+1][0], result_count[i+1][1])
+            list_tup.append(tup)
+            i += 2
+
+        cursor.close()
+        conn.close()
+
+        return list_tup
+    else:
+        list_standing = playerStandings()
+        i = 0
+        list_tup = []
+        while i < len(list_standing):            
+            tup = (list_standing[i][0], list_standing[i][1], list_standing[i+1][0], list_standing[i+1][1])
+            list_tup.append(tup)
+            i += 2
+
+        return list_tup
 
 if __name__ == '__main__':
-    deletePlayers();
+    print swissPairings()
